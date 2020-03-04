af71630d76d242c8cc4562dd382116546d9c63be
sejoonkim <sejoondev@gmail.com>
2020-03-04 23:14:26 +0900

    useReducer: can live outside component

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            throw new Error('Unhandled action');
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
+
+    const [number, dispatch] =useReducer(reducer, 0);
+
+    //const [number, setNumber] = useState(0);
 
     const onIncrease = () => {
-        setNumber(prevNumber => prevNumber + 1);
+        dispatch({
+            type: 'INCREMENT'
+        })
+        //setNumber(prevNumber => prevNumber + 1);
     }
     const onDecrease = () => {
-        setNumber(prevNumber => prevNumber - 1);
+        dispatch({
+            type: 'DECREMENT'
+        })
+        //setNumber(prevNumber => prevNumber - 1);
     }
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Counter from './Counter';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

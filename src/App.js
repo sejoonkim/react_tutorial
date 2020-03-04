2145b8c33d08ed77f4e8cdd5c2fdac9b10c406ae
sejoonkim <sejoondev@gmail.com>
2020-03-04 21:49:27 +0900

    useCallback Hook: function reuse, react dev tools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 // import Hello from './Hello';
 // import Wrapper from './Wrapper';
 //import Counter from './Counter';
@@ -20,13 +20,13 @@ function App() {
     email: '',
   });
   const { username, email } = inputs;
-  const onChange = e => {
+  const onChange = useCallback(e => {
     const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
-  }
+  }, [inputs]);
   // manage with useState
   const [users, setUsers] = useState([
     {
@@ -53,7 +53,7 @@ function App() {
   // nextId is managed by useRef()
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -68,20 +68,21 @@ function App() {
     });
 
     nextId.current += 1;
-  };
+  }, [username, email, users]);
 
 
-  const onRemove = id => {
+  const onRemove = useCallback(id => {
     setUsers(users.filter(user => user.id !== id));
-  };
+  }, [users]);
 
-  const onToggle = id => {
+  
+  const onToggle = useCallback(id => {
     setUsers(users.map(
       user => user.id === id
         ? { ...user, active: !user.active } // make a new object, copy original, then adjust
         : user
     ));
-  };
+  }, [users]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 

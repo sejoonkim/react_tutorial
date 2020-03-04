447e53a2a9e13d1d43d134d060ab39854d5018c6
sejoonkim <sejoondev@gmail.com>
2020-03-04 18:12:34 +0900

    basic JSX syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
+
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+    {/*comment*/}
+      <Hello 
+        // comment
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 

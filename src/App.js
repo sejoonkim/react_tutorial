3f2e4437484023b01b5aaf38ae717e07e53fa25a
sejoonkim <sejoondev@gmail.com>
2020-03-05 00:30:12 +0900

    When to use Immer?: check code readability

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 import useInputs from './useInputs';
+import produce from 'immer';
+
+window.produce = produce;
 
 function countActiveUsers(users) {
   console.log('counting active users...');
@@ -52,24 +55,35 @@ function reducer(state, action) {
     //     }
     //   };
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      })
+      // return {
+      //   inputs: initialState.inputs,
+      //   users: state.users.concat(action.user)
+      // };
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user => 
-          user.id === action.id
-          ? { ...user, active: !user.active}
-          : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      })  
+    // return {
+      //   ...state,
+      //   users: state.users.map(user => 
+      //     user.id === action.id
+      //     ? { ...user, active: !user.active}
+      //     : user
+      //   )
+      // };
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });  
+    // return {
+      //   ...state,
+      //   users: state.users.filter(user => user.id !== action.id)
+      // }
     default:
       throw new Error('Unhandled action');
   }
